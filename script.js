5c591d32326b0f4fdfea6a7cc8198bfa6bf2b1fb
Arjun cv <arjun7180@gmail.com>
2024-03-01 23:05:14 +0530

    finished assignment

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 const button = document.getElementById('likeButton');
-const icon = document.querySelector('.like-box span')
+const icon = document.querySelector('.like-box span');
+const likes = document.querySelector('.likes');
 
 
 button.addEventListener('click', like);
 
-function like{
+let count = 0;
 
-
+function like() {
+    likes.innerHTML = `${++count} likes`
+    icon.style.color = `rgb(15, 71, 192)`;
+    icon.style.animation = 'rotateAnimation 1s ease';
 }
